7d36e647fd26119ae53459b66c8ca8c3d09e7b30
deepikaksr <115573746+deepikaksr@users.noreply.github.com>
2025-06-16 17:11:43 +0530

    Deployment changes done

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/deepika-ksr-portfolio/",
   server: {
     host: "::",
     port: 8080,
